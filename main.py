2bf42cc10c748b2e025cbfe1f7eece7cbd3bb6b9
Clément Brun <clement.brun@mwm.io>
2025-05-23 17:07:15 +0200

    [main.py] update main with only ConversationService call

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from src.extraction.sources import extract_documents
-from src.embedding.ingestion import  safe_add_to_chromadb
-from src.clients.chromadb_client import reset_chromadb
 from src.services.conversation import ConversationService
 
-import logging
-
 if __name__ == "__main__":
-    collection_name = "relativitydb"
-    """
-    # Extracting documents and adding to ChromaDB
-
-    ## Extract documents from various sources
-    documents = extract_documents(source='url', url="https://fr.wikipedia.org/wiki/Relativit%C3%A9_restreinte", max_paragraphs=100)
-    #documents = extract_documents(source="gcs", bucket_name="mwm-cb-workspace",
-    #                              prefix="tiktok/html/generated/original/", suffix=".html",
-    #                              max_docs=100)
-    ## Add these documents (embedded) to our vector database
-    safe_add_to_chromadb(documents, collection_name)
-    """
+    collection_name = "tiktokdb"
     # Initializing
     conversation = ConversationService(collection_name=collection_name)
 
