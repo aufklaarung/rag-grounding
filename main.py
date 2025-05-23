bcb0b955beace7f74c5e03f704e84225dd097c2b
Clément Brun <clement.brun@mwm.io>
2025-05-23 17:00:41 +0200

    [main.py] entry point

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from src.extraction.sources import extract_documents
 from src.embedding.ingestion import  safe_add_to_chromadb
-from src.embedding.retriever import  get_best_matches
 from src.clients.chromadb_client import reset_chromadb
+from src.services.conversation import ConversationService
 
 import logging
 
 if __name__ == "__main__":
-    collection_name = "test"
-    # Extract documents from various sources
-    documents = extract_documents(source="gcs", bucket_name="mwm-cb-workspace",
-                                  prefix="tiktok/html/generated/original/", suffix=".html", max_docs=10)
-    #Add these documents (embedded) to our vector database
+    collection_name = "relativitydb"
+    """
+    # Extracting documents and adding to ChromaDB
+
+    ## Extract documents from various sources
+    documents = extract_documents(source='url', url="https://fr.wikipedia.org/wiki/Relativit%C3%A9_restreinte", max_paragraphs=100)
+    #documents = extract_documents(source="gcs", bucket_name="mwm-cb-workspace",
+    #                              prefix="tiktok/html/generated/original/", suffix=".html",
+    #                              max_docs=100)
+    ## Add these documents (embedded) to our vector database
     safe_add_to_chromadb(documents, collection_name)
-    # RAG time
-    results = get_best_matches("Qui est Albert Einstein?", collection_name)
+    """
+    # Initializing
+    conversation = ConversationService(collection_name=collection_name)
+
+    print("Bienvenue dans votre assistant conversationnel. Tapez 'exit' pour quitter.\n")
+
+    while True:
+        question = input("Vous: ").strip()
+        if question.lower() in {"exit", "quit"}:
+            print("Assistant: Au revoir !")
+            break
+
+        response = conversation.ask(question)
+        print("\nAssistant:\n" + response + "\n")
